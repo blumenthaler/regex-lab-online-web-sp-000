136677434e41a594c37fc735e7fb2ccf8b99f6ce
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-06-17 14:18:48 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,5 +23,9 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.match(/[0-9]{10}/)
+  if phone.match(/"?\(?\d{3}\)?\s?\-?\d{3}-?\s?\d{4}"?/)
+    true
+  else
+    false
+  end
 end
